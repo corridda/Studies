d761d919be7e9c6df3f6758898ea5ad423b4ab08
corridda <corridda@yandex.ru>
2020-12-08 17:03:52 +0300

    Books\Computer programming\Languages\Kotlin\Vermeulen_Functional_Programming_in_Kotlin_2020 initialization

---
+++ Books/Computer programming/Languages/Kotlin/Vermeulen_Functional_Programming_in_Kotlin_2020/__init__.py
@@ -0,0 +1,6 @@
+"""Vermeulen M. - Functional Programming in Kotlin (2020)
+*
+* m:\Книги\Научная и научно-познавательная литература\CS\
+* Computer Programming\Languages\Kotlin\authers\
+* Vermeulen M. - Functional Programming in Kotlin (2020)\
+"""
