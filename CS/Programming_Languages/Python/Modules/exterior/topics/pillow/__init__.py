c0bb734252c1b399cca5d49812210e3d2f1549a0
corridda <corridda@yandex.ru>
2021-08-26 17:41:52 +0300

    CS/Programming_Languages/Python/Modules/exterior/topics/pillow initialization

---
+++ CS/Programming_Languages/Python/Modules/exterior/topics/pillow/__init__.py
@@ -0,0 +1,4 @@
+"""Pillow is the friendly PIL fork by Alex Clark and Contributors.
+PIL is the Python Imaging Library by Fredrik Lundh and Contributors.
+https://pillow.readthedocs.io/en/stable/
+"""
---
+++ CS/Programming_Languages/Python/Modules/exterior/topics/pillow/pillow_usage.py
@@ -0,0 +1,28 @@
+"""Основные возможности библиотеки Python Imaging Library / Pillow / PIL
+https://pythonru.com/biblioteki/osnovnye-vozmozhnosti-biblioteki-python-imaging-library-pillow-pil
+"""
+
+from PIL import Image
+
+# Создание миниатюр
+
+filename = "Beeline_logo.jpg"
+
+size_1 = (32, 32)
+size_2 = (16, 16)
+
+try:
+    img = Image.open(filename)
+    print("Размер изображения:")
+    print(img.format, img.size, img.mode)
+    for size in [size_1, size_2]:
+        saved = None
+        if size == (32, 32):
+            saved = "Beeline_logo_32x32.jpg"
+        else:
+            saved = "Beeline_logo_16x16.jpg"
+        img.thumbnail(size)
+        img.save(saved)
+        img.show()
+except FileNotFoundError:
+    print("Файл не найден")
